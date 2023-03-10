5c624ce859305efbb5bbd503e70a494a23ac4a44
joaolucas04 <joaolucaspereiraramos04@gmail.com>
2023-03-10 07:57:56 -0300

    Adding dependencies

--- src/main/java/com/nnsei/course/resources/UserResource.java
+++ src/main/java/com/nnsei/course/resources/UserResource.java
@@ -1,5 +1,8 @@
 package com.nnsei.course.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,17 @@ import com.nnsei.course.entities.User;
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		User u1 = new User(1L, "Soyeon", "soyeon@gmail.com", "123456789", "soyeon123");
-		return ResponseEntity.ok().body(u1);
+		User u2 = new User(2L, "Jisoo", "jisoo@gmail.com", "99999999", "jsioobp");
+		User u3 = new User(3L, "Sowon", "sowon@gmail.com", "9123132", "gfriend123");
+		List<User> list = new ArrayList<>();
+		
+		list.add(u1);
+		list.add(u2);
+		list.add(u3);
+		
+		return ResponseEntity.ok().body(list);
 	}
 	
 	

9be5eaf565c4a6e7af99d88f1c9e580641f5081f
joaolucas04 <joaolucaspereiraramos04@gmail.com>
2023-03-11 12:13:43 -0300

    Adding order

--- src/main/java/com/nnsei/course/config/TestConfig.java
+++ src/main/java/com/nnsei/course/config/TestConfig.java
@@ -1,5 +1,7 @@
 package com.nnsei.course.config;
 
+import java.time.Instant;
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.nnsei.course.entities.Order;
 import com.nnsei.course.entities.User;
+import com.nnsei.course.repositories.OrderRepository;
 import com.nnsei.course.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +20,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -26,7 +33,15 @@ public class TestConfig implements CommandLineRunner {
 		User u5 = new User(null, "Umji", "umji@gmail.com", "13423098", "umjiviviz");
 		User u6 = new User(null, "Eunha", "eunha@gmail.com", "432890432", "eunha321");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o4 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u4);
+		Order o5 = new Order(null, Instant.parse("2019-01-21T03:42:10Z"), u5);
+		Order o6 = new Order(null, Instant.parse("2019-03-22T15:21:22Z"), u6);
 		
+		
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6));
 	} 
 }

2d1621701270d4da48c45b5eb07a4302c40e4682
joaolucas04 <joaolucaspereiraramos04@gmail.com>
2023-03-13 15:43:33 -0300

    Adding payment entity

--- src/main/java/com/nnsei/course/config/TestConfig.java
+++ src/main/java/com/nnsei/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nnsei.course.entities.Category;
 import com.nnsei.course.entities.Order;
 import com.nnsei.course.entities.OrderItem;
+import com.nnsei.course.entities.Payment;
 import com.nnsei.course.entities.Product;
 import com.nnsei.course.entities.User;
 import com.nnsei.course.entities.enums.OrderStatus;
@@ -77,11 +78,14 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:55:07Z"), o1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	} 
 }
